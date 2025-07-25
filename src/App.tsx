838b8afa5f1d8f0529fc82f8206c54a309771d6c
Pramuuu <pramodkatchalla28@gmail.com>
2025-07-25 09:18:02 +0530

    Added virtual chemistry lab project to f_pramod

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start p-4">
+      <h1 className="text-4xl font-bold text-blue-700 mb-8">Interactive Periodic Table</h1>
+      {/* Periodic Table will go here */}
+    </div>
+  );
+}
+
+export default App
